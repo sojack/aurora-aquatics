2e9f1a8c7850db31685fe9d5926b2e74b513a155
sojack <sojack@gmail.com>
2020-09-01 08:15:38 -0400

    update 404 message

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,10 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>
+      You just hit a route that doesn&#39;t exist... if you think this is an
+      error please contact us.
+    </p>
   </Layout>
 )
 
