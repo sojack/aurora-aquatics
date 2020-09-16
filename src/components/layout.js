49c9a038c6e8a7d40052a971c66e6e671c99e229
sojack <sojack@gmail.com>
2020-09-16 11:50:39 -0400

    remove header files

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,23 +8,11 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
-// import Header from "./header"
 import "./layout.scss"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
       <div>
         <main>{children}</main>
         <footer>
